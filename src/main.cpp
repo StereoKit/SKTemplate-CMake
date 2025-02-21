5a805be4c7c9d7f9e3057052cf810e2d92d79d44
Nick Klingensmith <programmerpichu@gmail.com>
2025-02-21 13:50:42 -0800

    Update to v0.3.10

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,47 @@
 #include <stereokit.h>
 #include <stereokit_ui.h>
+
+#include "floor.hlsl.h"
+
 using namespace sk;
+
+shader_t   floor_shader;
+material_t floor_mat;
+mesh_t     floor_mesh;
 
 mesh_t     cube_mesh;
 material_t cube_mat;
 pose_t     cube_pose = {{0,0,-0.5f}, quat_identity};
 
 int main(void) {
-    sk_settings_t settings = {};
-	settings.app_name           = "SKNativeTemplate";
-	settings.assets_folder      = "Assets";
-	settings.display_preference = display_mode_mixedreality;
+	sk_settings_t settings = {};
 	if (!sk_init(settings))
 		return 1;
 
-    cube_mesh = mesh_gen_rounded_cube(vec3_one * 0.1f, 0.02f, 4);
-    cube_mat  = material_find        (default_id_material_ui);
+	floor_mesh   = mesh_find        (default_id_mesh_cube);
+	floor_shader = shader_create_mem((void*)sks_floor_hlsl, sizeof(sks_floor_hlsl));
+	floor_mat    = material_create  (floor_shader);
+	material_set_transparency(floor_mat, transparency_blend);
 
-    sk_run([]() {
-        ui_handle_begin("Cube", cube_pose, mesh_get_bounds(cube_mesh), false);
-        render_add_mesh(cube_mesh, cube_mat, matrix_identity);
-        ui_handle_end();
+	cube_mesh = mesh_gen_rounded_cube(vec3_one * 0.1f, 0.02f, 4);
+	cube_mat  = material_find        (default_id_material_ui);
+
+	sk_run([]() {
+		if (device_display_get_type() == display_type_flatscreen) { 
+			mesh_draw(floor_mesh, floor_mat, matrix_ts({0,-1.5f,0}, {30,0.1f,30}));
+		}
+
+		ui_handle_begin("Cube", cube_pose, mesh_get_bounds(cube_mesh), false);
+		render_add_mesh(cube_mesh, cube_mat, matrix_identity);
+		ui_handle_end();
+
+	}, []() {
+		shader_release  (floor_shader);
+		mesh_release    (floor_mesh);
+		material_release(floor_mat);
+		mesh_release    (cube_mesh);
+		material_release(cube_mat);
 	});
 
-    return 0;
+	return 0;
 }
